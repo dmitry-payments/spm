2f30879942878d6d7c8f83acaf7a5d6564829280
dmitry-payments <72413287+dmitry-payments@users.noreply.github.com>
2024-03-14 18:37:03 +0300

    Update spm.sol

--- spm/contracts/spm.sol
+++ spm/contracts/spm.sol
@@ -12,6 +12,9 @@ contract AtomicSwapIERC20 {
     // Мапа для хранения информации о полученных деньгах
     mapping(address => uint256) public receivedAmounts;
 
+    // Мапа для хранения информации о приоритетных адресах для выплат
+    mapping(address => uint256) public priorityAddresses;
+
     // Награда контракта
     uint256 contractRevenue;
 
@@ -27,7 +30,8 @@ contract AtomicSwapIERC20 {
     // Функция, позволяющая контракту принимать деньги
     receive() external payable {
         // Записываем в мапу отправителя и сумму
-        receivedAmounts[msg.sender] += msg.value;
+        receivedAmounts[msg.sender] = msg.value;
+        totalAmount += msg.value;
         if (receivedAmounts.length > 0) {
             findAndSendRevenue(msg.sender);
         }
@@ -40,11 +44,43 @@ contract AtomicSwapIERC20 {
 
         // Проходим по мапе
         for (address previousAddress : receivedAmounts) {
-            // Получаем сумму отправителя
+
+            // Получаем сумму первого отправителя в мапе
             uint256 previousAmount = receivedAmounts[previousAddress];
 
             // Проверяем условие если сумма следующего сендера больше или равна предыдущего
             if (previousAmount <= lastSenderAmount && previousAmount != 0) {
+
+                // Проверяем есть ли приоритетные адреса для выплат
+                if (priorityAddresses.length > 0) {
+                    // Проходим по мапе приоритетных адресов
+                    for (address priorityAddress : priorityAddresses) {
+                        // Проверяем что общая сумма достаточна для выплаты приоритетному сендеру и награды контракта и что сендер активный
+                        if (totalAmount >= priorityAddresses[priorityAddress] * 2 && priorityAddresses[priorityAddress] != 0) {
+                            // Сумма выплаты
+                            uint256 amountToSend = priorityAddresses[priorityAddress] + (priorityAddresses[priorityAddress] * 80 / 100);
+                            // Награда контракту
+                            contractRevenue = priorityAddresses[priorityAddress] * 20 / 100;
+                            //Делаем неактивным адрес приоритетного отправителя
+                            priorityAddresses[priorityAddress] = 0;
+                        }
+                    }
+                } 
+
+                // Рассчитываем сумму для отправки, добавляя 80% к previousAmount
+                uint256 amountToSend = previousAmount + (previousAmount * 80 / 100);
+
+                contractRevenue = previousAmount * 20 / 100;
+
+                // Отправляем эфир на адрес предыдущего отправителя
+                payable(previousAddress).transfer(amountToSend);
+
+                //Делаем неактивным адрес предыдущего отправителя
+                receivedAmounts[previousAddress] = 0;
+            }
+
+            // Проверяем условие если сумма следующего сендера меньше предыдущего, но общая сумма контракта позволяет сделать выплату
+            if (previousAmount > lastSenderAmount && totalAmount >= previousAmount) {
 
                 // Рассчитываем сумму для отправки, добавляя 80% к previousAmount
                 uint256 amountToSend = previousAmount + (previousAmount * 80 / 100);
@@ -58,16 +94,11 @@ contract AtomicSwapIERC20 {
                 receivedAmounts[previousAddress] = 0;
             }
 
-            // Проверяем условие если сумма следующего сендера меньше предыдущего
-            if (previousAmount > lastSenderAmount && totalAmount >= previousAmount) {
+            // Проверяем условие если сумма следующего сендера меньше предыдущего, но общая сумма контракта НЕ позволяет сделать выплату
+            if (previousAmount > lastSenderAmount && totalAmount < previousAmount) {
 
-                // Рассчитываем сумму для отправки, добавляя 80% к previousAmount
-                uint256 amountToSend = previousAmount + (previousAmount * 80 / 100);
-
-                contractRevenue = previousAmount * 20 / 100
-
-                // Отправляем эфир на адрес предыдущего отправителя
-                payable(previousAddress).transfer(amountToSend);
+                // Добавляем адрес в приоритетную мапу для первостепенных выплат
+                priorityAddresses[previousAddress] = previousAmount;
             }
         }
     }
